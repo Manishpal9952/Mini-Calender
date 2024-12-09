d2d525527e37da2259ea8ae91452a112d90eb5bf
Manish Pal <palmansh44@gmail.com>
2024-12-10 01:02:34 +0530

    Add files via upload

---
+++ Mini Calendar.js
@@ -0,0 +1,15 @@
+let day = document.getElementById('day');
+let date =  document.getElementById('date');
+let month = document.getElementById('month');
+let year = document.getElementById('year');
+
+let today = new Date()
+
+let allDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+let allMonths = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+date.innerHTML = (today.getDate()<10?"0":"") + today.getDate();
+day.innerHTML = allDays[today.getDay()];
+month.innerHTML = allMonths[today.getMonth()];
+year.innerHTML = today.getFullYear();
